2d9a21b107ae8f0daaa340ef30228d7094365d1a
rspitler <rspitler@bbf43d1b-8368-41bb-ab07-b6fd7f518bce>
2012-08-22 20:37:35 +0000

    Updated IDF and Preprocessor.

--- branches/EnergyPlus/src/PreprocessInput.f90
+++ branches/EnergyPlus/src/PreprocessInput.f90
@@ -18,6 +18,7 @@ CONTAINS
         ! backup the last input file
         call system('if exist in.idf rename in.idf inBackup.idf')
         ! rename the heat pump input file in preparation for epmacro
+        !call system('copy E+_hpdata.idf in.imf')
         call system('rename E+_hpdata.idf in.imf')
         !call system('rename hpdata_longtubes.idf in.imf')
         !call system('rename HPdata_3TonAC_410.idf in.imf')
