48cdc4ddb02041ddf1df11e5e0ecb94557b86fa9
jholman <john.holman@okstate.edu>
2012-12-29 17:34:45 +0000

    Header completed for UnitConvert.f90, see ticket #60

--- src/UnitConvert.f90
+++ src/UnitConvert.f90
@@ -5,60 +5,62 @@
 ! ************************************** !
 ! -- HIGH LEVEL OVERVIEW/DESCRIPTION --- !
 ! -------------------------------------- !
-! Provide a 1 or 2 sentence overview of this module.  
-! In most cases, it is probably not a useful entry and can be inferred from the name of the module anyway.
+! This module converts the input units to standard SI units 
 !
 ! ************************************** !
 ! -- PHYSICAL DESCRIPTION -------------- !
 ! -------------------------------------- !
-! This component represents something...or nothing...in a heat pump system.
-! A description of the component is found at:
-! some website
-! From that website: 
-!  - It does something
+! THis does not represent a physical component of the heat pump
 
 ! ************************************** !
 ! -- SIMULATION DATA RESPONSIBILITIES -- !
 ! -------------------------------------- !
-! Here's a one line summary of what this does for the simulation itself.
-! This module takes inputs such as...and modifies them like so...and outputs these things
-
+! THis module takes the parameters for each of the components and converts the inputs to SI units
+! 
 ! ************************************** !
 ! -- INPUT FILES/OUTPUT FILES (none) --- !
 ! -------------------------------------- !
-! Check for any OPEN statements in the code
-! Check for any WRITE statements in the code
-!  Note that writing to unit "*" or "6" means just write to the terminal, not to a file
-
+! no associated files
+!
 ! ************************************** !
 ! -- MODULE LEVEL VARIABLES/STRUCTURES - !
 ! -------------------------------------- !
-! What vars and structures are defined at the *module* level...are units defined?  Any other notes?
+! Constants defined at the module level
+!   UnitPwr, UnitAirFlw, UnitRfFlw, UnitP, UnitM, UnitL, UnitK
+! Flags defined at module level
+!   SI, IP
 
 ! ************************************** !
 ! -- SUMMARY OF METHODS, CALL TREE ----- !
 ! -------------------------------------- !
 ! This module contains X methods:
-!    PUBLIC InitSomething -- What does this do (in one line)?
-!      Called by what other modules?
+!    PUBLIC UnitConvert
+!       ORNLSolver
+!    PUBLIC MicroChannelUnitConvert
+!    PUBLIC FinTubeCoilUnitConvert
+!       Condenser
+!       Evaporator
+!    PUBLIC Temperature_F2C
+!       ORNLSolver
+!       AirTempLoop
+!    PUBLIC Temperature_C2F
+
 
 ! ************************************** !
 ! -- ISSUES/BUGS/TICKETS --------------- !
 ! -------------------------------------- !
-! Are there any interesting issues with this, unfuddle ticket numbers?
+! This module is to be removed, according to Ticket #23
 
 ! ************************************** !
 ! -- CHANGELOG ------------------------- !
 ! -------------------------------------- !
 ! 2012-12-11 | ESL | Initial header
-! YEAR-MM-DD | ABC | Some other log message? 
+! 2012-12-29 | JEH | Header completion
 
 ! ************************************** !
 ! -- TODO/NOTES/RECOMMENDATIONS -------- !
 ! -------------------------------------- !
-! Put some notes for what needs to happen here
-! Silly things are fine
-! Somethings these small silly things are great to grab on to when starting up with the code after being off for a while
+! this module should be removed 
 
 MODULE UnitConvertMod
 
