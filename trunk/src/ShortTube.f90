a81ebcc50d6fc65eea643495b98106908b83c679
jholman <jholman@bbf43d1b-8368-41bb-ab07-b6fd7f518bce>
2012-12-29 18:18:20 +0000

    Finished Header for ShortTube.f90, see ticket #58.  Started to complete header for ZEROCH.f90, but could not determine its purpose in the program, see ticket #64

--- trunk/src/ShortTube.f90
+++ trunk/src/ShortTube.f90
@@ -5,60 +5,59 @@
 ! ************************************** !
 ! -- HIGH LEVEL OVERVIEW/DESCRIPTION --- !
 ! -------------------------------------- !
-! Provide a 1 or 2 sentence overview of this module.  
-! In most cases, it is probably not a useful entry and can be inferred from the name of the module anyway.
+! THis module calculates the flow rate through a short tube orifice.  There are 3 different methods for this, empirical, Choi, and Payne. 
 !
 ! ************************************** !
 ! -- PHYSICAL DESCRIPTION -------------- !
 ! -------------------------------------- !
-! This component represents something...or nothing...in a heat pump system.
-! A description of the component is found at:
-! some website
-! From that website: 
-!  - It does something
+! THis does not represent a physical component of the HEat Pump system
 
 ! ************************************** !
 ! -- SIMULATION DATA RESPONSIBILITIES -- !
 ! -------------------------------------- !
-! Here's a one line summary of what this does for the simulation itself.
-! This module takes inputs such as...and modifies them like so...and outputs these things
-
+! THis module takes the expansion device inlet conditions and the outlet conditions from the compressor and the inlet conditions to the evaporator.
+! These inputs are used to calculate the outlet conditions of the expansion device.
+!
 ! ************************************** !
 ! -- INPUT FILES/OUTPUT FILES (none) --- !
 ! -------------------------------------- !
-! Check for any OPEN statements in the code
-! Check for any WRITE statements in the code
-!  Note that writing to unit "*" or "6" means just write to the terminal, not to a file
+! no input/output files
 
 ! ************************************** !
 ! -- MODULE LEVEL VARIABLES/STRUCTURES - !
 ! -------------------------------------- !
-! What vars and structures are defined at the *module* level...are units defined?  Any other notes?
+! Parameters defined:
+!       PI, UnitL
+! Variables Defined
+!       PiExpDev, HiExpDev, TiExpDev, PoExpDev, HoExpDev, ToExpDev
 
 ! ************************************** !
 ! -- SUMMARY OF METHODS, CALL TREE ----- !
 ! -------------------------------------- !
 ! This module contains X methods:
-!    PUBLIC InitSomething -- What does this do (in one line)?
-!      Called by what other modules?
+!    PUBLIC ShortTube - Calculates the outlet conditions using a semi-empirical model.
+!       FlowRateLoop
+!       HPdesignMod
+!    PUBLIC ShortTubeChoi - Calculates the outlet conditions using Buckingham-Pi theorem
+!    PUBLIC ShortTubePayne - Calculates the outlet conditions using Buckingham-Pi
+!       FlowRateLoop
+!       HPDesignMod
 
 ! ************************************** !
 ! -- ISSUES/BUGS/TICKETS --------------- !
 ! -------------------------------------- !
-! Are there any interesting issues with this, unfuddle ticket numbers?
+! NONE
 
 ! ************************************** !
 ! -- CHANGELOG ------------------------- !
 ! -------------------------------------- !
 ! 2012-12-11 | ESL | Initial header
-! YEAR-MM-DD | ABC | Some other log message? 
+! 2012-12-29 | JEH | Header Completion 
 
 ! ************************************** !
 ! -- TODO/NOTES/RECOMMENDATIONS -------- !
 ! -------------------------------------- !
-! Put some notes for what needs to happen here
-! Silly things are fine
-! Somethings these small silly things are great to grab on to when starting up with the code after being off for a while
+! Do we need to retain any subroutines not used in the program?
 
     MODULE ShortTubeMod
 
