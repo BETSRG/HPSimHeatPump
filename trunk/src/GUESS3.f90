db096d89e4488cf9d7ec2083319281a696206fc7
karthik <karthik@bbf43d1b-8368-41bb-ab07-b6fd7f518bce>
2014-01-14 18:16:07 +0000

    Release Notes - 1) Fixed up the issues in current Brent Method Implementation. 2) Updated the comments section to give some related sources for further study on Brent Method (Future) 3) Increased the Current Tolerance Level (Testing Feature-  goal is to improve the accuracy of solver results) 4) Introduced the Machine Precision Mode (Testing Feature - Will be improved in future if required) 5) Updated the Variable Names in the GUESS3.f90 Method, for easy understanding during debugging. 6) Validated the Implementation of the Brent Method with some sample equations and shared the results with the team via Email.

--- trunk/src/GUESS3.f90
+++ trunk/src/GUESS3.f90
@@ -38,7 +38,7 @@
 ! -- ISSUES/BUGS/TICKETS --------------- !
 ! -------------------------------------- !
 ! A better look needs to be taken at why the root bracketing is exponential.
-! "X2 = X2 + 2.0**(ICOUNT-1)*DX*SIGN"
+! "upperBoundValueGuess = upperBoundValueGuess + 2.0**(ICOUNT-1)*DX*SIGN"
 
 ! ************************************** !
 ! -- CHANGELOG ------------------------- !
@@ -46,21 +46,49 @@
 ! 2012-12-11 | ESL | Initial header
 ! 2012-12-29 | JEH | Filled out header
 ! 2013-12-18 | RAS | Updated Issues & To-Do
+! 2014-1-14 | Karthik | Updated Variable Names and comments
 
 ! ************************************** !
 ! -- TODO/NOTES/RECOMMENDATIONS -------- !
 ! -------------------------------------- !
 ! The root bracketing needs to be looked at. In addition, some documentation would be good.
 
- SUBROUTINE GUESS3(X1,Y1,X2,Y2,SUBA,DX,TOL,IERROR)
+
+!      /*---------------------------------------------------------------------
+!        |  METHOD: GUESS3
+!        |
+!        |  PURPOSE:  Guess the Other Bound Value for the Solver (Usually UpperBound Value)
+!        |     
+!        |  Pre-condition: 
+!        |
+!        |  Post-condition: 
+!        |
+!        |  PARAMETERS:
+!        |            lowerBoundValueGuess - Lower Bound Value for the Function 
+!        |            FunctionValForLowerBoundGuess - F(X) Value for the LowerBound Value
+!        |            upperBoundValueGuess -  Upper Bound Value to be Guessed  
+!        |            FunctionValForUpperBoundGuess -  F(X) Value for the Upper Bound Value
+!        |            FunctionPointerAddress -   Address to the Method to be Called
+!        |            DX - Step Value 
+!                     TOL - Tolerence Value  
+!        |            IERROR - Error Code
+!        |
+!        |  Returns: Upper and Lower Bound Value to Calling Method (ZeroConvergence). By Modifying the Input parameters values
+!        | 
+!        | LAST UPDATED - Karthik | 1/14/2014 | Updated the Naming of Variables and comments.
+!        *-------------------------------------------------------------------*/
+
+
+ 
+ SUBROUTINE GUESS3(lowerBoundValueGuess,FunctionValForLowerBoundGuess,upperBoundValueGuess,FunctionValForUpperBoundGuess,FunctionPointerAddress,DX,TOL,IERROR)
       implicit none
 
       INTEGER IERR, ICOUNT, IERROR
       REAL SIGN, SLOPE
-      REAL X1, X2, Y1, Y2, DX, TOL
+      REAL lowerBoundValueGuess, upperBoundValueGuess, FunctionValForLowerBoundGuess, FunctionValForUpperBoundGuess, DX, TOL
       REAL YMIN, DDX
-      REAL SUBA
-        EXTERNAL SUBA
+      REAL FunctionPointerAddress
+        EXTERNAL FunctionPointerAddress
       CHARACTER(LEN=111),PARAMETER :: FMT_1001 = "(' GUESS3: ** FAILED TO BRACKET A SOLUTION **',/,9X,'F(',1PE12.4,') = ',1PE13.5,5X,'F(',1PE12.4,') = ',1PE13.5)"
 
       ICOUNT = 0
@@ -69,16 +97,16 @@
       SLOPE = 0.0
 
       DO WHILE (.TRUE.)
-          Y1 = SUBA(X1,IERR)
-          X2 = X1                                !Initialize ISI - 03/26/04
+          FunctionValForLowerBoundGuess = FunctionPointerAddress(lowerBoundValueGuess,IERR)
+          upperBoundValueGuess = lowerBoundValueGuess                                !Initialize ISI - 03/26/04
           ICOUNT = ICOUNT + 1
           IF (IERR .EQ. 0) THEN
               EXIT
           END IF
           IF (IERR .EQ. 1) THEN                  !ISI - 03/26/04
-              X1 = X1 - DX                         !ISI - 03/26/04
+              lowerBoundValueGuess = lowerBoundValueGuess - DX                         !ISI - 03/26/04
           ELSE                                   !ISI - 03/26/04
-              X1 = X1 + DX                         !ISI - 03/26/04
+              lowerBoundValueGuess = lowerBoundValueGuess + DX                         !ISI - 03/26/04
           END IF                                 !ISI - 03/26/04
           
           IF (ICOUNT .GT. 30) THEN
@@ -88,23 +116,23 @@
 
       END DO
 
-      YMIN = ABS(Y1)
+      YMIN = ABS(FunctionValForLowerBoundGuess)
       IF (YMIN .LE. TOL) THEN
           RETURN              !ISI - 02/12/06
       END IF
-      IF(Y1.GT.0.0) THEN
+      IF(FunctionValForLowerBoundGuess.GT.0.0) THEN
           SIGN = -1.0
       END IF
       DDX = ABS(DX)
-      X2 = X2 + 2.0**(ICOUNT-1)*DX*SIGN      !To bracket root ISI - 03/26/04
+      upperBoundValueGuess = upperBoundValueGuess + 2.0**(ICOUNT-1)*DX*SIGN      !To bracket root ISI - 03/26/04
       
       DO WHILE (.TRUE.)
 
-          Y2 = SUBA(X2,IERR)    !RS Comment: The compressor saturation temperature seems to be iterating the wrong way with the MC case
+          FunctionValForUpperBoundGuess = FunctionPointerAddress(upperBoundValueGuess,IERR)    !RS Comment: The compressor saturation temperature seems to be iterating the wrong way with the MC case
           ICOUNT = ICOUNT + 1
-          SLOPE = (Y2 - Y1)/(X2 - X1)
+          SLOPE = (FunctionValForUpperBoundGuess - FunctionValForLowerBoundGuess)/(upperBoundValueGuess - lowerBoundValueGuess)
           IF (IERR .NE. 0) THEN 
-              X2 = (X1 + X2)/2.
+              upperBoundValueGuess = (lowerBoundValueGuess + upperBoundValueGuess)/2.
               IF (ICOUNT .GT. 25) THEN  !15) THEN   !RS: Debugging: Trying to let it iterate more
                   IERROR = 4
                   RETURN
@@ -113,14 +141,14 @@
               CYCLE
           END IF
 
-          YMIN = AMIN1(YMIN,ABS(Y2))
+          YMIN = AMIN1(YMIN,ABS(FunctionValForUpperBoundGuess))
           IF (YMIN .LE. TOL) THEN
               RETURN              !ISI - 02/12/06
           END IF
           DDX = ABS(DX)
           DDX=2.0**(ICOUNT-1)*ABS(DX)				!ISI - 05/10/04
 !
-          IF (Y1*Y2 .LE. 0.) THEN
+          IF (FunctionValForLowerBoundGuess*FunctionValForUpperBoundGuess .LE. 0.) THEN
               RETURN
           END IF
           IF (ICOUNT .GT. 15) THEN
@@ -132,17 +160,17 @@
 !	IF SLOPE IS NEGATIVE AND "Y" IS LESS THAN 0. MOVE THE LOWER
 !	POINT TO THE LEFT
 !
-          SLOPE = (Y2 - Y1)/(X2 - X1)
-          IF (SLOPE*Y1 .GE. 0.) THEN
+          SLOPE = (FunctionValForUpperBoundGuess - FunctionValForLowerBoundGuess)/(upperBoundValueGuess - lowerBoundValueGuess)
+          IF (SLOPE*FunctionValForLowerBoundGuess .GE. 0.) THEN
 !
-!	MAKE "X1" THE LOWER OR LEFT-HAND POINT
+!	MAKE "lowerBoundValueGuess" THE LOWER OR LEFT-HAND POINT
 !
-              IF (X1 .GE. X2) THEN
-                  X1 = X2
-                  Y1 = Y2
+              IF (lowerBoundValueGuess .GE. upperBoundValueGuess) THEN
+                  lowerBoundValueGuess = upperBoundValueGuess
+                  FunctionValForLowerBoundGuess = FunctionValForUpperBoundGuess
               END IF
               
-              X2 = X1 - DDX
+              upperBoundValueGuess = lowerBoundValueGuess - DDX
               CYCLE
 
           END IF
@@ -151,15 +179,15 @@
 !	THERE IS A POSITIVE SLOPE AND "Y" IS LESS THAN 0. THEN
 !	MOVE THE RIGHT-HAND POINT FURTHER TO THE RIGHT
 !
-          IF (X1 .LE. X2) THEN 
+          IF (lowerBoundValueGuess .LE. upperBoundValueGuess) THEN 
 !
-!	MAKE X1 THE UPPER POINT
+!	MAKE lowerBoundValueGuess THE UPPER POINT
 !
-              X1 = X2
-              Y1 = Y2
+              lowerBoundValueGuess = upperBoundValueGuess
+              FunctionValForLowerBoundGuess = FunctionValForUpperBoundGuess
           END IF
 
-          X2 = X1 + DDX
+          upperBoundValueGuess = lowerBoundValueGuess + DDX
           CYCLE   !VL: Not needed ...
 
       END DO
